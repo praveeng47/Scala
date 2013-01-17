fc0dc87f9189b0df751bf40252c42a14292e03fa
Praveen Kumar Gade <praveeng47@gmail.com>
2013-01-17 19:02:50 +0100

    first commit

---
+++ Idea Project/src/Praveen/HelloScala.scala
@@ -0,0 +1,17 @@
+package Praveen
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Praveen
+ * Date: 1/17/13
+ * Time: 2:15 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+object HelloScala {
+  def main(args: Array[String])
+  {
+    println("Hello World")
+  }
+
+}
